0859d8a2f77bdd6a160dc7bd013ebf5afe7eaf4d
Tung Vu <tung@xprotocol.org>
2022-04-09 02:45:58 +0700

    run script every time restarting

--- src/aws.js
+++ src/aws.js
@@ -7,24 +7,80 @@ function buildUserDataScript(githubRegistrationToken, label) {
   if (config.input.runnerHomeDir) {
     // If runner home directory is specified, we expect the actions-runner software (and dependencies)
     // to be pre-installed in the AMI, so we simply cd into that directory and then start the runner
-    return [
-      '#!/bin/bash',
-      `cd "${config.input.runnerHomeDir}"`,
-      'export RUNNER_ALLOW_RUNASROOT=1',
-      `./config.sh --url https://github.com/${config.githubContext.owner}/${config.githubContext.repo} --token ${githubRegistrationToken} --labels ${label}`,
-      './run.sh',
-    ];
+    return `Content-Type: multipart/mixed; boundary="//"
+MIME-Version: 1.0
+
+--//
+Content-Type: text/cloud-config; charset="us-ascii"
+MIME-Version: 1.0
+Content-Transfer-Encoding: 7bit
+Content-Disposition: attachment; filename="cloud-config.txt"
+
+#cloud-config
+cloud_final_modules:
+- [scripts-user, always]
+
+--//
+Content-Type: text/x-shellscript; charset="us-ascii"
+MIME-Version: 1.0
+Content-Transfer-Encoding: 7bit
+Content-Disposition: attachment; filename="userdata.txt"
+
+#!/bin/bash
+cd "${config.input.runnerHomeDir}"
+export RUNNER_ALLOW_RUNASROOT=1
+echo 'Configuring runner'
+./config.sh \
+  --url https://github.com/${config.githubContext.owner}/${config.githubContext.repo} \
+  --token ${githubRegistrationToken} \
+  --labels ${label} \
+  --work _work \
+  --ephemeral
+echo 'Starting runner'
+./run.sh
+--//--`;
   } else {
-    return [
-      '#!/bin/bash',
-      'mkdir actions-runner && cd actions-runner',
-      'case $(uname -m) in aarch64) ARCH="arm64" ;; amd64|x86_64) ARCH="x64" ;; esac && export RUNNER_ARCH=${ARCH}',
-      'curl -O -L https://github.com/actions/runner/releases/download/v2.286.0/actions-runner-linux-${RUNNER_ARCH}-2.286.0.tar.gz',
-      'tar xzf ./actions-runner-linux-${RUNNER_ARCH}-2.286.0.tar.gz',
-      'export RUNNER_ALLOW_RUNASROOT=1',
-      `./config.sh --url https://github.com/${config.githubContext.owner}/${config.githubContext.repo} --token ${githubRegistrationToken} --labels ${label}`,
-      './run.sh',
-    ];
+    return `Content-Type: multipart/mixed; boundary="//"
+MIME-Version: 1.0
+
+--//
+Content-Type: text/cloud-config; charset="us-ascii"
+MIME-Version: 1.0
+Content-Transfer-Encoding: 7bit
+Content-Disposition: attachment; filename="cloud-config.txt"
+
+#cloud-config
+cloud_final_modules:
+- [scripts-user, always]
+
+--//
+Content-Type: text/x-shellscript; charset="us-ascii"
+MIME-Version: 1.0
+Content-Transfer-Encoding: 7bit
+Content-Disposition: attachment; filename="userdata.txt"
+
+#!/bin/bash
+export RUNNER_ALLOW_RUNASROOT=1
+if [ ! -d "./actions-runner" ]; then
+  echo 'Installing runner'
+  mkdir -p actions-runner
+  cd actions-runner
+  case $(uname -m) in aarch64) ARCH="arm64" ;; amd64|x86_64) ARCH="x64" ;; esac && export RUNNER_ARCH=$\{ARCH\}
+  curl -O -L https://github.com/actions/runner/releases/download/v2.286.0/actions-runner-linux-$\{RUNNER_ARCH\}-2.286.0.tar.gz
+  tar xzf ./actions-runner-linux-$\{RUNNER_ARCH\}-2.286.0.tar.gz
+else
+  cd actions-runner
+fi
+echo 'Configuring runner'
+./config.sh \
+  --url https://github.com/${config.githubContext.owner}/${config.githubContext.repo} \
+  --token ${githubRegistrationToken} \
+  --labels ${label} \
+  --work _work \
+  --ephemeral
+echo 'Starting runner'
+./run.sh
+--//--`;
   }
 }
 
@@ -38,7 +94,7 @@ async function startEc2Instance(label, githubRegistrationToken) {
     InstanceType: config.input.ec2InstanceType,
     MinCount: 1,
     MaxCount: 1,
-    UserData: Buffer.from(userData.join('\n')).toString('base64'),
+    UserData: Buffer.from(userData).toString('base64'),
     SubnetId: config.input.subnetId,
     SecurityGroupIds: [config.input.securityGroupId],
     IamInstanceProfile: { Name: config.input.iamRoleName },
@@ -60,10 +116,10 @@ async function startEc2Instance(label, githubRegistrationToken) {
       tagsFilters.push({ Name: `tag:${tag.Key}`, Values: [tag.Value] });
     }
     const describeParams = {
-      Filters: tagsFilters,
+      Filters: [...tagsFilters, { Name: 'instance-state-name', Values: ['stopped'] }],
     };
 
-    core.info(`Checking for hibernated instance with filter ${JSON.stringify(describeParams)}`);
+    core.info(`Checking for stopped instance with filter ${JSON.stringify(describeParams)}`);
 
     try {
       const result = await ec2.describeInstances(describeParams);
