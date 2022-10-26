edce690cd4d0044dda7f24ff5802b010fafcdcd4
Tung Vu <tung@finbloxapp.com>
2022-10-26 20:22:28 +0700

    Drop from root

--- src/aws.js
+++ src/aws.js
@@ -25,37 +25,43 @@ Content-Transfer-Encoding: 7bit
 Content-Disposition: attachment; filename="userdata.txt"
 
 #!/bin/bash
+set -x
+
 function start_runner {
-  cd "$\{HOME_DIR\}/actions-runner/runner_$\{1\}"
+  cd "$\{HOME\}/actions-runner/runner_$\{1\}"
   echo "Getting token to get metadata of EC2 instance"
   TOKEN=$(curl -X PUT "http://169.254.169.254/latest/api/token" -H "X-aws-ec2-metadata-token-ttl-seconds: 21600")
   echo Getting ec2 instance id
-  INSTANCE_ID=$(curl -H "X-aws-ec2-metadata-token: $\{TOKEN\}" -v http://169.254.169.254/latest/meta-data/instance-id)
+  export INSTANCE_ID=$(curl -H "X-aws-ec2-metadata-token: $\{TOKEN\}" -v http://169.254.169.254/latest/meta-data/instance-id)
   echo "Got instance id $\{INSTANCE_ID\}"
-
+  export RUNNER_NAME="$\{INSTANCE_ID\}_runner_$\{1\}"
+  echo "Runner name is $\{RUNNER_NAME\}"
   echo "Getting runner token"
-  RUNNER_TOKEN=$(curl -s -XPOST \
+  export RUNNER_TOKEN=$(curl -s -XPOST \
     -H "authorization: token ${githubToken}" \
     https://api.github.com/repos/${config.githubContext.owner}/${config.githubContext.repo}/actions/runners/registration-token | \
     jq -r .token)
   if [ -f ".runner" ]; then
     echo Unregistering old runner data
-    ./config.sh remove --token $RUNNER_TOKEN
+    su -p "action-user" -c bash -c "./config.sh remove --token $\{RUNNER_TOKEN\}"
   fi
   echo "Registering runner"
-  ./config.sh \
+  su -p "action-user" -c bash -c './config.sh \
     --url https://github.com/${config.githubContext.owner}/${config.githubContext.repo} \
-    --token $RUNNER_TOKEN \
-    --labels "$\{INSTANCE_ID\},$\{INSTANCE_ID\}_runner_$\{1\}" \
-    --name "$\{INSTANCE_ID\}_runner_$\{1\}"
+    --token $\{RUNNER_TOKEN\} \
+    --labels "$\{INSTANCE_ID\},$\{RUNNER_NAME\}" \
+    --name "$\{RUNNER_NAME\}"'
 
   echo "Starting runner"
-  ./run.sh
+  su -p "action-user" -c bash -c "./run.sh --unattended"
 }
 
-export RUNNER_ALLOW_RUNASROOT=1
-export HOME_DIR=$(pwd)
-if [ ! -d "$\{HOME_DIR\}/actions-runner" ]; then
+export HOME="/home/action-user"
+if [ ! -d "$\{HOME\}/actions-runner" ]; then
+  mkdir -p $HOME
+  groupadd "action-user"
+  useradd -d $HOME -g "action-user" "action-user"
+  echo "action-user ALL=(ALL) NOPASSWD:ALL" >> /etc/sudoers.d/action-user-sudo-no-passwd
   command -v yum >/dev/null 2>&1 \
     && { echo "Installing dependencies with yum"; \
       sudo yum -y install libicu60 jq git; }
@@ -65,16 +71,20 @@ if [ ! -d "$\{HOME_DIR\}/actions-runner" ]; then
       sudo apt-get install -y jq git; }
   curl -fsSL https://get.docker.com -o get-docker.sh; \
     sudo sh get-docker.sh;
+  echo "Setup docker for non-root user"
+  groupadd docker
+  usermod -aG docker "action-user"
   echo "Installing runner"
-  mkdir -p actions-runner
-  cd actions-runner
+  mkdir -p $\{HOME\}/actions-runner
+  cd $\{HOME\}/actions-runner
   case $(uname) in Darwin) OS="osx" ;; Linux) OS="linux" ;; esac && export RUNNER_OS=$\{OS\}
   case $(uname -m) in aarch64|arm64) ARCH="arm64" ;; amd64|x86_64) ARCH="x64" ;; esac && export RUNNER_ARCH=$\{ARCH\}
   curl -O -L "https://github.com/actions/runner/releases/download/v2.298.2/actions-runner-$\{RUNNER_OS\}-$\{RUNNER_ARCH\}-2.298.2.tar.gz"
   for i in $(seq 1 ${runnerCount}); do
-    mkdir -p "$\{HOME_DIR\}/actions-runner/runner_$\{i\}"
-    tar xzf "./actions-runner-linux-$\{RUNNER_ARCH\}-2.298.2.tar.gz" -C "$\{HOME_DIR\}/actions-runner/runner_$\{i\}"
+    mkdir -p "$\{HOME\}/actions-runner/runner_$\{i\}"
+    tar xzf "./actions-runner-linux-$\{RUNNER_ARCH\}-2.298.2.tar.gz" -C "$\{HOME\}/actions-runner/runner_$\{i\}"
   done
+  chown -R "action-user:action-user" $HOME
 fi
 
 for i in $(seq 1 ${runnerCount}); do
